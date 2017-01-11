1a5f37f7c8b114ae0dcf8f77e34ef4cd12b6eb2e
Fässler, Christian <cfaessler@ins.hsr.ch>
2017-01-11 15:39:35 +0100

    Add WIP

--- main.py
+++ main.py
@@ -1,24 +1,62 @@
 import urequests
 import time
-from machine import Pin
+from machine import Pin,  RTC
+from settings import Settings
+from ntplib import NTPClient
 
 API_KEY = "123"
 NUMBER_OF_3HRS_WINDOWS = 2
 
 
-def do_connect():
-    import network
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect('wireless', 'massiveloosing')
-        while not wlan.isconnected():
-            pass
-    print('network config:', wlan.ifconfig())
+class Main(object):
+    def __init__(self):
+        self.settings = Settings('settings.json')
+                
+    def configure(self):
+        print('Config menu')
+        print('Type c to configure device, otherwise just wait')
+        read_data = input()
+
+        if 'c' in read_data:
+            wifi_password = input('WiFi Password: ')
+            ssid = input('Enter SSID: ')
+            self.settings.set('ssid',  ssid)
+            self.settings.set('password',  wifi_password)
+        else:
+            print('No configuration changes')
+        print('Booting')
 
 
-if __name__ == "__main__":
+    def do_connect():
+        import network
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        if not wlan.isconnected():
+            ssid = self.settings.get('ssid')
+            password = self.settings.get('password')
+            wlan.connect(ssid, password)
+            print('Connecting to network %s...' % ssid)
+            while not wlan.isconnected():
+                pass
+            print('Connected to %s' % ssid)
+        print('network config:', wlan.ifconfig())
+        
+    def setup_time():
+        ntp_client = NTPClient()
+        resp = ntp_client.request('0.ch.pool.ntp.org',  version=3,  port=123)
+        rtc = RTC()
+        rtc.init(time.localtime(time.time() + resp.offset))   
+
+
+if __name__ == '__main__':
+    main = Main()
+    #main.do_connect()
+    #main.setup_time()
+    main.configure()
+    
+    
+    """"
+        
     do_connect()
     dictionary = {num: False for num in range(NUMBER_OF_3HRS_WINDOWS)}
 
@@ -50,4 +88,4 @@ if __name__ == "__main__":
             pin = Pin(k + 3, Pin.OUT)
             pin.high if k else pin.low()
         print(dictionary)
-        time.sleep(10)
+        time.sleep(10)"""
